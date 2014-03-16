b937e4ab2e51869a468bfff9dce8f0a45ef2f5e3
Chris <christiane.okamoto@gmail.com>
2014-03-15 23:50:34 -0300

    tenta fazer wicegrid funcionar no heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,10 @@ NugapSystem::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   #config.assets.compile = false
   config.assets.compile = true
+  config.cache_classes = true
+config.serve_static_assets = true
+config.assets.compile = true
+config.assets.digest = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
